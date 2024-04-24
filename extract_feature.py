a2a848bffd6e82fb04465b7f02a258bf2094f2a3
Ketut Kusuma <71806737+KetutKusuma@users.noreply.github.com>
2024-04-24 15:26:56 +0800

    done bang

--- extract_feature.py
+++ extract_feature.py
@@ -1,12 +1,20 @@
 import librosa
 from sklearn.preprocessing import MinMaxScaler
 
+scaler = MinMaxScaler()
+
 
 class mfccAlg:
     # untuk mengekstrak fitur dengan metode mfcc
     def extract_mfcc(self, y):
         mfccs = librosa.feature.mfcc(y=y, n_mfcc=13)
-        scaler = MinMaxScaler()
         normalized = scaler.fit_transform(mfccs)
 
+        return normalized.T.flatten()
+
+    # ekstrak dengan filepath
+    def extract_mfcc_with_filepath(self, path):
+        y, sr = librosa.load(path=path)
+        mfccs = librosa.feature.mfcc(y=y, n_mfcc=13)
+        normalized = scaler.fit_transform(mfccs)
         return normalized.T.flatten()
